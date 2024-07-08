11ce3d369d148e6fd0de7e2b1442b619eda55e45
Asazin007 <asim80822@gmail.com>
2024-07-08 14:05:07 +0530

    layouts m=Tweet model

--- TweetApp/TweetApp/settings.py
+++ TweetApp/TweetApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tweet',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'TweetApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -127,5 +129,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS= [os.path.join(BASE_DIR,'static')]
--- TweetApp/tweet/urls.py
+++ TweetApp/tweet/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     # path('', admin.site.urls),
+    path('', views.index,name='index'),
 ]
