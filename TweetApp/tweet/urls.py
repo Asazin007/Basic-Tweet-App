6933d44837337a21efa497877013c9ae8c62134b
Asazin007 <asim80822@gmail.com>
2024-07-08 19:25:24 +0530

    views , urls, and template with routing

--- TweetApp/tweet/urls.py
+++ TweetApp/tweet/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     # path('', admin.site.urls),
-    path('', views.index,name='index'),
+    path('', views.tweet_list,name='tweet_list'),
+    path('create/', views.tweet_create,name='tweet_create'),
+    path('<int:tweet_id>/edit/', views.tweet_edit,name='tweet_edit'),
+    path('<int:tweet_id>/delete/', views.tweet_delete,name='tweet_delete'),
 ]
--- TweetApp/tweet/views.py
+++ TweetApp/tweet/views.py
@@ -1,5 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TweetForm
+from .models import Tweet
 
-# Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('index')  # Change 'index' to the appropriate URL name for your index page
+    else:
+        form = TweetForm()
+    
+    return render(request, 'index.html', {'form': form})
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    
+    return render(request, 'tweet_form.html', {'form': form})
+
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    
+    return render(request, 'tweet_form.html', {'form': form})
+
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
