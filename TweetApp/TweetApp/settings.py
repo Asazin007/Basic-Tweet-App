689ba683a6cdee4ebcab993cc00b1824a7d6150f
Asazin007 <asim80822@gmail.com>
2024-07-09 02:54:13 +0530

    complete crud

--- TweetApp/TweetApp/settings.py
+++ TweetApp/TweetApp/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL ='/accounts/login'
+LOGIN_REDIRECT_URL = '/tweet/'
+LOGOUT_REDIRECT_URL = '/tweet/'
+
--- TweetApp/tweet/views.py
+++ TweetApp/tweet/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import TweetForm
+from .forms import *
 from .models import Tweet
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 def index(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
@@ -19,6 +21,7 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
+@login_required
 def tweet_create(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
@@ -32,6 +35,7 @@ def tweet_create(request):
     
     return render(request, 'tweet_form.html', {'form': form})
 
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method == 'POST':
@@ -45,7 +49,7 @@ def tweet_edit(request, tweet_id):
         form = TweetForm(instance=tweet)
     
     return render(request, 'tweet_form.html', {'form': form})
-
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method == 'POST':
@@ -53,3 +57,18 @@ def tweet_delete(request, tweet_id):
         return redirect('tweet_list')
     
     return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request,user)
+            return redirect('login')           
+
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'registration/register.html', {'form': form})
